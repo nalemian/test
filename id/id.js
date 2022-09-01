b2d521965c0e2824d80c389edd66d18e75b8645d
nalemian <adelianagimova@gmail.com>
2022-09-01 18:03:27 +0500

    ищет элементы по id

---
+++ id/id.js
@@ -0,0 +1,8 @@
+var text=document.getElementById("text");
+text.title="New text";
+console.log(text.title); 
+
+text.style.color="red";
+text.style.backgroundColor="blue";
+
+text.innerHTML="New<br>string";
