4e1dadecadcb43bc50b1497efdc2ba79f1bc15e9
nalemian <adelianagimova@gmail.com>
2022-09-24 14:17:07 +0500

    ограничено количество символов, стили отдельно

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,9 +1,11 @@
 var counter=0;
 function onClickButton(element) {
-    var text=document.getElementById("mainButton");
+    if(counter<=8) {
+        var text=document.getElementById("mainButton");
     if(counter==0 && element.innerHTML!==",") {
         text.innerHTML= element.innerHTML
     } else
     text.innerHTML = text.innerHTML+element.innerHTML;
     counter++;
+    }
 }
