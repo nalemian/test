4a6e9e12a49e126c9ad4857a76ad744931330301
nalemian <adelianagimova@gmail.com>
2022-09-25 16:16:14 +0500

    отдельный стиль для кнопки и для 4-х колонн

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,7 +1,7 @@
 var counter=0;
 function onClickButton(element) {
-    if(counter<=8) {
-        var text=document.getElementById("mainButton");
+    if(counter<=180) {
+        var text=document.getElementById("output");
     if(counter==0 && element.innerHTML!==",") {
         text.innerHTML= element.innerHTML
     } else
