cef0391163c1060f89814ee1f16b14e7771eb016
nalemian <adelianagimova@gmail.com>
2022-10-01 20:03:01 +0500

    привязка к длине содержимого, без счётчика

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,10 @@
-var counter=0;
 function onClickButton(element) {
-    if(counter<=180) {
-        var text=document.getElementById("output");
-    if(counter==0 && element.innerHTML!==",") {
-        text.innerHTML= element.innerHTML
-    } else
-    text.innerHTML = text.innerHTML+element.innerHTML;
-    counter++;
+    var stringContent = document.getElementById("output");
+    var buttonContent = element.innerHTML;
+    if (stringContent.innerHTML.length <= 180) {
+        if (stringContent.innerHTML.length == 1 && buttonContent !== ",") {
+            stringContent.innerHTML = buttonContent;
+        } else
+            stringContent.innerHTML = stringContent.innerHTML + buttonContent;
     }
 }
