f2bc8a770eb1c5cb72312c5670416e052b9a6bcc
nalemian <adelianagimova@gmail.com>
2022-10-04 20:30:42 +0500

    заготовка

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -8,3 +8,44 @@ function onClickButton(element) {
             stringContent.innerHTML = stringContent.innerHTML + buttonContent;
     }
 }
+
+var plus =  {
+ "operation": "+",
+ "apply": function (num1,num2){
+  return num1 + num2;
+ }
+}
+var minus =  {
+    "operation": "+",
+    "apply": function (num1,num2){
+     return num1 - num2;
+    }
+   }
+var multiply =  {
+ "operation": "+",
+ "apply": function (num1,num2){
+  return num1 * num2;
+ }
+}
+var divide =  {
+    "operation": "+",
+    "apply": function (num1,num2){
+     return num1 / num2;
+    }
+   }
+
+let oper=undefined;
+let number1=undefined;
+let arr=[plus, minus, multiply, divide];
+function operations(model) {
+    oper=model;
+    number1=document.getElementById("output").innerHTML;
+}
+
+function equal() {
+    number2=document.getElementById("output").innerHTML;
+    for(let i=0; i<4; i++) {
+        if(oper.innerHTML == arr[i].operation)
+            arr[i].apply(number1, number2)
+    }
+}
