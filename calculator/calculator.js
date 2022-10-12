2970e68d563b0ebe79569c23070525580dd9e1a9
nalemian <adelianagimova@gmail.com>
2022-10-12 19:41:55 +0500

    не так много ||, использую map, но = не работает

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,55 +2,55 @@ function onClickButton(element) {
     var stringContent = document.getElementById("output");
     var buttonContent = element.innerHTML;
     if (stringContent.innerHTML.length <= 180) {
-        if (( stringContent.innerHTML=="+" || stringContent.innerHTML=="-" || stringContent.innerHTML=="*" || stringContent.innerHTML=="/") || (stringContent.innerHTML == "0" && buttonContent !== ",")) {
+        if ((isNaN(Number(stringContent.innerHTML)))==true || (stringContent.innerHTML == "0" && buttonContent !== ",")) {
             stringContent.innerHTML = buttonContent;
-        } else
+        } else {
             stringContent.innerHTML = stringContent.innerHTML + buttonContent;
-    }
+        }
+}
 }
 
 function reset() {
     document.getElementById("output").innerHTML="0";
 }
 
- var plus =  {
- "operation": "+",
- "apply": function (num1,num2){
-  return num1 + num2;
- }
-}
-var minus =  {
-    "operation": "-",
-    "apply": function (num1,num2){
-     return num1 - num2;
-    }
-   }
-var multiply =  {
- "operation": "*",
- "apply": function (num1,num2){
-  return num1 * num2;
- }
-}
-var divide =  {
-    "operation": "/",
-    "apply": function (num1,num2){
-     return num1 / num2;
-    }
-   }
-
 let oper=undefined;
 let number1=undefined;
-let arr=[plus, minus, multiply, divide];
-function operations(model) {
-    oper=model;
-    number1=document.getElementById("output").innerHTML;
-    document.getElementById("output").innerHTML=oper.innerHTML;
-}
 
+function operations(oper) {
+    value=document.getElementById("output");
+    number1=value.innerHTML;
+    value.innerHTML=oper;
+}
+ let Hashmap=new Map([
+    [1,  plus =  {
+        "operation": "+",
+        "apply": function (num1,num2){
+         return num1 + num2;
+        }
+       }],
+    [2, minus =  {
+        "operation": "-",
+        "apply": function (num1,num2){
+         return num1 - num2;
+        }
+       }],
+    [3, multiply =  {
+        "operation": "*",
+        "apply": function (num1,num2){
+         return num1 * num2;
+        }
+       }],
+    [4, divide =  {
+        "operation": "/",
+        "apply": function (num1,num2){
+         return num1 / num2;
+        }
+       }]
+ ]) 
 function equal() {
-    number2=document.getElementById("output");
-    for(let i=0; i<4; i++) {
-        if(oper.innerHTML == arr[i].operation)
-            number2.innerHTML=arr[i].apply(Number(number1), Number(number2.innerHTML))
+    for(let i=1; i<=Hashmap.size; i++) {
+        if(oper == Hashmap.get(i).operation)
+        document.getElementById("output").innerHTML=Hashmap.get(i).apply(Number(number1), Number(document.getElementById("output").innerHTML))
     }
-} 
+}
