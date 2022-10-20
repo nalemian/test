0b1f3840c4d0acd416d43220af359f71fdd04cff
nalemian <adelianagimova@gmail.com>
2022-10-20 20:23:38 +0500

    пункты 1-4, 9 готовы

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,16 +2,32 @@ function onClickButton(element) {
     var stringContent = document.getElementById("output");
     var buttonContent = element.innerHTML;
     if (stringContent.innerHTML.length <= 180) {
-        if (((isNaN(Number(stringContent.innerHTML))) && (stringContent.innerHTML.indexOf(',')==-1)) || (stringContent.innerHTML == "0" && buttonContent !== ",")) {
-            stringContent.innerHTML = buttonContent;
-        } else {
-            stringContent.innerHTML = stringContent.innerHTML + buttonContent;
+        if(buttonContent=='.' && stringContent.innerHTML.indexOf('.')!==-1) {} else {
+            if((isNaN(Number(stringContent.innerHTML))) && buttonContent=='.') {
+                stringContent.innerHTML='0.'; }
+        else {
+             if (((isNaN(Number(stringContent.innerHTML))) && (stringContent.innerHTML.indexOf('.')==-1)) || (stringContent.innerHTML == "0" && buttonContent !== ".")) {
+                    stringContent.innerHTML = buttonContent;
+                }
+            else {
+                stringContent.innerHTML = stringContent.innerHTML + buttonContent;
+            }
         }
-}
+    }
+    }
 }
 
 function reset() {
     document.getElementById("output").innerHTML="0";
+}
+
+function del() {
+    let res=document.getElementById('output');
+    if(res.innerHTML.length==1) {
+        res.innerHTML='0';
+    } else {
+        res.innerHTML=res.innerHTML.substr(0, res.innerHTML.length-1);
+    }
 }
 
 let operation=undefined;
@@ -45,5 +61,7 @@ function operations(oper) {
 
 function equal() {
     let total=document.getElementById("output");
-    total.innerHTML=map.get(operation).apply(Number(number1), Number(total.innerHTML));
+    if(!isNaN(number1)) {
+    total.innerHTML=map.get(operation).apply(Number(number1), Number(total.innerHTML)); } 
+    else {total.innerHTML='0'};
 }
