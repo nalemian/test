f44d5b2763526bcfb7fbdf3e292aa4efe04683f1
nalemian <adelianagimova@gmail.com>
2022-10-22 17:55:03 +0500

    все пункты, отображение текущего результата

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,6 +1,12 @@
+let bool=false;
+let counter=0;
 function onClickButton(element) {
     var stringContent = document.getElementById("output");
     var buttonContent = element.innerHTML;
+    if(bool) {
+        document.getElementById('top').innerHTML=stringContent.innerHTML; 
+        bool=false;
+    }
     if (stringContent.innerHTML.length <= 180) {
         if(buttonContent=='.' && stringContent.innerHTML.indexOf('.')!==-1) {} else {
             if((isNaN(Number(stringContent.innerHTML))) && buttonContent=='.') {
@@ -18,11 +24,16 @@ function onClickButton(element) {
 }
 
 function reset() {
-    document.getElementById("output").innerHTML="0";
+    bool=false;
+    let res=document.getElementById('output');
+    document.getElementById('top').innerHTML=res.innerHTML;
+    res.innerHTML="0";
 }
 
 function del() {
+    bool=false;
     let res=document.getElementById('output');
+    document.getElementById('top').innerHTML=res.innerHTML;
     if(res.innerHTML.length==1) {
         res.innerHTML='0';
     } else {
@@ -34,10 +45,19 @@ let operation=undefined;
 let number1=undefined;
 
 function operations(oper) {
+    bool=false;
     value=document.getElementById("output");
+    res=document.getElementById('top');
+    if(counter>0) {
+        operEqual();
+        number1=res.innerHTML;
+    } else {
+    res.innerHTML=value.innerHTML;
     number1=value.innerHTML;
+    }
     operation=oper;
     value.innerHTML=oper;
+    counter++;
 }
 
  let map = new Map();
@@ -60,8 +80,16 @@ function operations(oper) {
    });
 
 function equal() {
+    counter=0;
     let total=document.getElementById("output");
     if(!isNaN(number1)) {
-    total.innerHTML=map.get(operation).apply(Number(number1), Number(total.innerHTML)); } 
+        res=map.get(operation).apply(Number(number1), Number(total.innerHTML));
+        document.getElementById('output').innerHTML=res;
+    bool=true;
+} 
     else {total.innerHTML='0'};
+}
+function operEqual () {
+    total=document.getElementById("output");
+    document.getElementById('top').innerHTML=map.get(operation).apply(Number(number1), Number(total.innerHTML));
 }
