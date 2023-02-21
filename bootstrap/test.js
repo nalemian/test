d69bca67b446b3ccad1fb147215dc2b345d0711c
nalemian <adelianagimova@gmail.com>
2023-02-21 22:18:03 +0500

    переадресация на калькулятор двух видов

---
+++ bootstrap/test.js
@@ -0,0 +1,7 @@
+function onClickButton(el) {
+    window.location = "file:///D:/%D0%90%D0%B4%D0%B5%D0%BB%D1%8F%20%D1%83%D1%87%D1%91%D0%B1%D0%B0/repo/test-2/calculator/calculator.html"
+}
+function onClick(el) {
+    document.getElementById('frame').style.display='block';
+    document.getElementById('img').style.display='none';
+}
